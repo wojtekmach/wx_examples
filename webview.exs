ef6ade704663bfe9a12e16d9f8ac88feae812ac6
Wojtek Mach <wojtek@wojtekmach.pl>
2022-04-02 23:34:37 +0200

    Update webview.exs

--- webview.exs
+++ webview.exs
@@ -1,12 +1,17 @@
 wx = :wx.new()
 
-f = :wxFrame.new(wx, -1, "Demo", pos: {50, 50}, size: {800, 600})
-:wxFrame.show(f)
-
+f = :wxFrame.new(wx, -1, "Demo 1", pos: {50, 50}, size: {800, 600})
 mb = :wxMenuBar.new()
 :wxFrame.setMenuBar(f, mb)
 :wxMenuBar.connect(mb, :command_menu_selected, skip: true)
+:wxFrame.show(f)
+:wxWebView.new(f, -1, url: "https://elixir-lang.org", size: {800, 600})
 
+f = :wxFrame.new(wx, -1, "Demo 2", pos: {100, 100}, size: {800, 600})
+mb = :wxMenuBar.new()
+:wxFrame.setMenuBar(f, mb)
+:wxMenuBar.connect(mb, :command_menu_selected, skip: true)
+:wxFrame.show(f)
 :wxWebView.new(f, -1, url: "https://elixir-lang.org", size: {800, 600})
 
 receive do
