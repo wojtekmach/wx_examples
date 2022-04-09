aea2ff52fd8994be88ea4a618571e86ff447c952
Wojtek Mach <wojtek@wojtekmach.pl>
2022-04-09 12:30:37 +0200

    Update webview.exs

--- webview.exs
+++ webview.exs
@@ -1,20 +1,67 @@
 wx = :wx.new()
+size = {800, 600}
+f = :wxFrame.new(wx, -1, "Window", size: size)
 
-f = :wxFrame.new(wx, -1, "Demo 1", pos: {50, 50}, size: {800, 600})
-mb = :wxMenuBar.new()
-:wxFrame.setMenuBar(f, mb)
-:wxMenuBar.connect(mb, :command_menu_selected, skip: true)
-:wxFrame.show(f)
-:wxWebView.new(f, -1, url: "https://elixir-lang.org", size: {800, 600})
-
-f = :wxFrame.new(wx, -1, "Demo 2", pos: {100, 100}, size: {800, 600})
-mb = :wxMenuBar.new()
-:wxFrame.setMenuBar(f, mb)
-:wxMenuBar.connect(mb, :command_menu_selected, skip: true)
-:wxFrame.show(f)
-:wxWebView.new(f, -1, url: "https://elixir-lang.org", size: {800, 600})
-
-receive do
-  event ->
-    IO.inspect(event)
+if :os.type() == {:unix, :darwin} do
+  # create a new menu bar to handle built-in "quit" menu item
+  mb = :wxMenuBar.new()
+  :wxFrame.setMenuBar(f, mb)
 end
+
+:wxFrame.connect(f, :close_window, skip: true)
+:wxFrame.connect(f, :command_menu_selected, skip: true)
+:wxFrame.show(f)
+
+webview = :wxWebView.new(f, -1, url: "https://elixir-lang.org", size: size)
+:wxWebView.connect(webview, :webview_newwindow)
+:wxWebView.connect(webview, :webview_navigating)
+:wxWebView.connect(webview, :webview_loaded)
+:wxWebView.connect(webview, :webview_error)
+
+defmodule Loop do
+  require Logger
+
+  @wxID_EXIT 5006
+
+  def loop do
+    receive do
+      {:wx, @wxID_EXIT, _, _, _} = event ->
+        Logger.debug(inspect(event))
+        :ok
+
+      {:wx, _, _webview, _, {:wxWebView, :webview_navigating, _, _, _, 'http://wx/quit'}} = event ->
+        Logger.debug(inspect(event))
+        :ok
+
+      {:wx, _, _webview, _, {:wxWebView, :webview_navigating, _, _, _, _}} = event ->
+        Logger.debug(inspect(event))
+        loop()
+
+      {:wx, _, webview, _, {:wxWebView, :webview_loaded, _, _, _, _url}} = event ->
+        Logger.debug(inspect(event))
+
+        js = """
+        var handleDocumentKeyDown = function(event) {
+          const isMacOS = /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform);
+          const cmd = isMacOS ? event.metaKey : event.ctrlKey;
+          const shift = event.shiftKey;
+          const key = event.key;
+
+          if (cmd && key === "q") {
+            window.location.replace('http://wx/quit');
+          }
+        }
+        document.addEventListener("keydown", handleDocumentKeyDown);
+        """
+
+        {true, _} = :wxWebView.runScript(webview, js)
+        loop()
+
+      event ->
+        Logger.debug(inspect(event))
+        loop()
+    end
+  end
+end
+
+Loop.loop()
